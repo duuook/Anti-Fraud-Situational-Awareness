a09aded71529dfc273c12b304e643153788dacff
duuook <3022531349@qq.com>
2024-06-06 18:36:17 +0800

    移除无用app01文件夹

--- SE/utils/db_modelform.py
+++ SE/utils/db_modelform.py
@@ -4,6 +4,7 @@ from SE import models
 
 class PhoneNumberModelForm(forms.ModelForm):
     """电话号码表表单"""
+
     class Meta:
         model = models.phone_number
         fields = ['id', '电话号码', '电话类型', '标记次数']
@@ -16,6 +17,7 @@ class PhoneNumberModelForm(forms.ModelForm):
 
 class msgModelForm(forms.ModelForm):
     """短信表表单"""
+
     class Meta:
         model = models.msg
         fields = ['id', '短信类别', '短信内容']
@@ -24,3 +26,17 @@ class msgModelForm(forms.ModelForm):
     #     super(msgModelForm, self).__init__(*args, **kwargs)
     #     for name, field in self.fields.items():
     #         field.widget.attrs = {'class': 'form-control'}
+
+
+class WebsiteModelForm(forms.ModelForm):
+    """网站表表单"""
+
+    class Meta:
+        model = models.website
+        fields = ['id', '网站域名', '诈骗类型']
+
+    def __init__(self, *args, **kwargs):
+        super(WebsiteModelForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs = {'class': 'form-control'}
+
