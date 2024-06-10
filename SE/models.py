8cd9171905b4f09921d6c756eda47c409a5b894d
duuook <3022531349@qq.com>
2024-06-10 11:03:07 +0800

    设置诈骗类型外键

--- SE/models.py
+++ SE/models.py
@@ -22,8 +22,23 @@ class 诈骗类型说明(models.Model):
 # 主表
 class website(models.Model):
     网站域名 = models.CharField(max_length=100)
-    诈骗类型 = models.ForeignKey(to="诈骗类型说明", to_field="诈骗类型id", default=None, null=True, blank=True,
-                                 on_delete=models.SET_NULL)
+    # 诈骗类型=models.ForeignKey(to="诈骗类型说明",to_field="诈骗类型id",default=None,null=True,blank=True,on_delete=models.SET_NULL)
+    诈骗类型_CHOICES = (
+        (0, '正常'),
+        (1, '购物消费'),
+        (2, '婚恋交友'),
+        (3, '假冒身份'),
+        (4, '钓鱼网站'),
+        (5, '冒充公检法'),
+        (6, '平台诈骗'),
+        (7, '招聘兼职'),
+        (8, '杀猪盘'),
+        (9, '博彩赌博'),
+        (10, '信贷理财'),
+        (11, '刷单诈骗'),
+        (12, '中奖诈骗'),
+    )
+    诈骗类型 = models.IntegerField(default=None, null=True, blank=True, choices=诈骗类型_CHOICES)
 
 
 # 短信端口——短信表
@@ -31,3 +46,8 @@ class msg(models.Model):
     id = models.IntegerField(primary_key=True)
     短信类别 = models.SmallIntegerField()
     短信内容 = models.CharField(max_length=255)
+
+
+# 电子邮箱——电子邮箱表
+class email(models.Model):
+    电子邮箱地址 = models.CharField(max_length=50)
