011916ed361c96364139394a2c68d2b9d27c6a3f
duuook <3022531349@qq.com>
2024-06-12 00:06:36 +0800

    新增分页以及筛选功能

--- SE/views.py
+++ SE/views.py
@@ -57,9 +57,96 @@ def fraud_msg_list(request):
 def fraud_ip_list(request):
     """防诈态势感知-诈骗IP列表"""
     if request.method == 'GET':
-        form = WebsiteModelForm()
-        websites = models.website.objects.all()
-        return render(request, 'fraud_ip_list.html', {'form': form, 'websites': websites})
+        # 获取筛选数据：
+        # sip为筛选IP，stype为筛选类型
+        type_choices = models.website.诈骗类型_CHOICES
+        filter_ip_condition = request.GET.get('sip', '')
+        filter_type_condition = request.GET.get('stype', '')
+
+        # 分页设计:
+        #   每页都是十条数据，根据前端传递的页码进行分页
+        page = int(request.GET.get('page', 1))
+        page_size = 10
+        start = (page - 1) * page_size
+        end = page * page_size
+
+        if filter_type_condition != '0' and filter_type_condition != '':
+            filter_ip_content = models.website.objects.filter(网站域名__contains=filter_ip_condition,
+                                                              诈骗类型=filter_type_condition)[start:end]
+            total_count = models.website.objects.filter(网站域名__contains=filter_ip_condition,
+                                                        诈骗类型=filter_type_condition).count()
+        else:
+            filter_ip_content = models.website.objects.filter(网站域名__contains=filter_ip_condition)[start:end]
+            total_count = models.website.objects.filter(网站域名__contains=filter_ip_condition).count()
+
+        # 页码
+        # 1. 总页码
+        total_page, m = divmod(total_count, page_size)
+        if m:
+            total_page += 1
+        print(total_page)
+
+        # 2. 生成页码
+        #   只显示前五页和后五页
+        if total_page < 11:
+            page_start = 1
+            page_end = total_page + 1
+        else:
+            if page < 6:
+                page_start = 1
+                page_end = 11
+            else:
+                if page + 5 > total_page:
+                    page_start = total_page - 9
+                    page_end = total_page + 1
+                else:
+                    page_start = page - 5
+                    page_end = page + 6
+
+        page_str_list = []
+
+        # 首页
+        first = '<li><a href="?page=1"><span aria-hidden="true">首页</span></a></li>'
+        page_str_list.append(first)
+
+        # 上一页
+        if page == 1:
+            prev = '<li class="disabled"><a href="#"><span aria-hidden="true">&laquo;</span></a></li>'
+        else:
+            prev = '<li><a href="?page=%s"><span aria-hidden="true">&laquo;</span></a></li>' % (page - 1)
+        page_str_list.append(prev)
+
+        for i in range(page_start, page_end):
+            if i == page:
+                temp = '<li class="active"><a href="?page=%s">%s</a></li>' % (i, i)
+            else:
+                temp = '<li><a href="?page=%s">%s</a></li>' % (i, i)
+            page_str_list.append(temp)
+
+        # 下一页
+        if page == total_page:
+            after = '<li class="disabled"><a href="#"><span aria-hidden="true">&raquo;</span></a></li>'
+        else:
+            after = '<li><a href="?page=%s"><span aria-hidden="true">&raquo;</span></a></li>' % (page + 1)
+        page_str_list.append(after)
+
+        # 尾页
+        last = '<li><a href="?page=%s"><span aria-hidden="true">尾页</span></a></li>' % total_page
+        page_str_list.append(last)
+
+        page_str = mark_safe(''.join(page_str_list))
+
+        """
+        <li><a href="?page=2#">2</a></li
+        <li><a href="?page=3">3</a></li>
+        <li><a href="?page=4">4</a></li>
+        <li><a href="?page=5">5</a></li>
+        """
+
+        return render(request, 'fraud_ip_list.html',
+                      {'websites': filter_ip_content, 'filter_condition': filter_ip_condition,
+                       'type_choices': type_choices, 'page_str': page_str, 'total_page': total_page,
+                       'current_page': page})
 
 
 def fraud_email_list(request):
