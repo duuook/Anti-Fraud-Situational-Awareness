5c82b43cec9682af81611151855c4fa88e8bebd6
duuook <3022531349@qq.com>
2024-06-13 00:05:57 +0800

    封装分页组件，下一步给所有信息库页面添加分页功能

--- SE/utils/pagination.py
+++ SE/utils/pagination.py
@@ -1,9 +1,128 @@
 from SE import models
-
-"""
-    自定义分页组件
-"""
+from django.utils.safestring import mark_safe
+import copy
 
 
 class Pagination(object):
-    pass
+    """
+        自定义分页组件,使用它需要传入request对象和筛选后的数据
+            page: 当前页码
+            total_page: 总页码
+            filter_ip_content: 筛选后的数据
+            page_str: 生成的页码
+    """
+
+    def __init__(self, request, filter_ip_content, page_size=10, page_param="page", plus=5):
+        """
+        :param request: 请求体
+        :param filter_ip_content: 筛选后的数据
+        :param page_size: 一次展示的总页数
+        :param page_param: 前端传递的页码参数
+        :param plus: 单边展示的页码数
+        """
+
+        self.query_dict = copy.deepcopy(request.GET)
+        self.query_dict._mutable = True
+
+        self.page_param = page_param
+
+        page = request.GET.get(page_param, "1")
+        if page.isdecimal():
+            page = int(page)
+        else:
+            page = 1
+
+        # 获取筛选数据：
+        self.page = page
+        self.page_size = page_size
+        self.start = (page - 1) * page_size
+        self.end = page * page_size
+
+        # 获取数据库总条数以及筛选后的数据
+        total_count = filter_ip_content.count()
+        self.filter_ip_content = filter_ip_content[self.start:self.end]
+
+        # 总页码
+        total_page, m = divmod(total_count, page_size)
+        if m:
+            total_page += 1
+        self.total_page = total_page
+
+        self.plus = plus
+
+    def html(self):
+        # 2. 生成页码
+        #   只显示前五页和后五页
+        if self.total_page < 2 * self.plus + 1:
+            page_start = 1
+            page_end = self.total_page + 1
+        else:
+            if self.page < self.plus + 1:
+                page_start = 1
+                page_end = 2 * self.plus + 1
+            else:
+                if self.page + self.plus > self.total_page:
+                    page_start = self.total_page - 2 * self.plus + 1
+                    page_end = self.total_page + 1
+                else:
+                    page_start = self.page - self.plus
+                    page_end = self.page + self.plus + 1
+
+        page_str_list = []
+
+        # 首页
+        self.query_dict[self.page_param] = 1
+        first = '<li><a href="?{}"><span aria-hidden="true">首页</span></a></li>'.format(self.query_dict.urlencode())
+        page_str_list.append(first)
+
+        # 上一页
+        if self.page == 1:
+            prev = '<li class="disabled"><a href="#"><span aria-hidden="true">&laquo;</span></a></li>'
+        else:
+            prev = '<li><a href="?page=%s"><span aria-hidden="true">&laquo;</span></a></li>' % (self.page - 1)
+        page_str_list.append(prev)
+
+        for i in range(page_start, page_end):
+            if i == self.page:
+                self.query_dict[self.page_param] = i
+                temp = '<li class="active"><a href="?%s">%s</a></li>' % (self.query_dict.urlencode(), i)
+            else:
+                self.query_dict[self.page_param] = i
+                temp = '<li><a href="?%s">%s</a></li>' % (self.query_dict.urlencode(), i)
+            page_str_list.append(temp)
+
+        # 下一页
+        if self.page == self.total_page:
+            after = '<li class="disabled"><a href="#"><span aria-hidden="true">&raquo;</span></a></li>'
+        else:
+            self.query_dict[self.page_param] = self.page + 1
+            after = '<li><a href="?%s"><span aria-hidden="true">&raquo;</span></a></li>' % (self.query_dict.urlencode())
+        page_str_list.append(after)
+
+        # 尾页
+        self.query_dict[self.page_param] = self.total_page
+        last = '<li><a href="?%s"><span aria-hidden="true">尾页</span></a></li>' % (self.query_dict.urlencode())
+        page_str_list.append(last)
+
+        """
+        <li><a href="?page=2#">2</a></li
+        <li><a href="?page=3">3</a></li>
+        <li><a href="?page=4">4</a></li>
+        <li><a href="?page=5">5</a></li>
+        """
+
+        # 跳转框
+        jump = """<li>
+                   <form method="get" style="float: left;margin-left: -1px">
+                       <input name="page"
+                              style="position: relative;float: left;display: inline-block;width: 110px;border-radius:0 "
+                              type="number" class="form-control" placeholder="页码">
+                       <button id="jump-button" class="btn btn-default" type="submit">跳转</button>
+                   </form>
+               </li>
+               """
+        page_str_list.append(jump)
+
+        page_str = mark_safe(''.join(page_str_list))
+
+        return page_str
--- SE/views.py
+++ SE/views.py
@@ -1,7 +1,8 @@
+import copy
+
 from django.shortcuts import render, redirect, HttpResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
-from django.utils.safestring import mark_safe
 from SE.utils.db_modelform import *  # 这里的前驱路径需要完整打出
 from SE.utils.pagination import Pagination
 
@@ -64,102 +65,30 @@ def fraud_ip_list(request):
         filter_ip_condition = request.GET.get('sip', '')
         filter_type_condition = request.GET.get('stype', '')
 
-        # 分页设计:
-        #   每页都是十条数据，根据前端传递的页码进行分页
-        page = int(request.GET.get('page', 1))
-        page_size = 10
-        start = (page - 1) * page_size
-        end = page * page_size
-
         # 获取数据库总条数以及筛选后的数据
         if filter_type_condition != '0' and filter_type_condition != '':
             filter_ip_content = models.website.objects.filter(网站域名__contains=filter_ip_condition,
-                                                              诈骗类型=filter_type_condition)[start:end]
+                                                              诈骗类型=filter_type_condition)
             total_count = models.website.objects.filter(网站域名__contains=filter_ip_condition,
                                                         诈骗类型=filter_type_condition).count()
         else:
-            filter_ip_content = models.website.objects.filter(网站域名__contains=filter_ip_condition)[start:end]
+            filter_ip_content = models.website.objects.filter(网站域名__contains=filter_ip_condition)
             total_count = models.website.objects.filter(网站域名__contains=filter_ip_condition).count()
 
-        # 页码
-        # 1. 总页码
-        total_page, m = divmod(total_count, page_size)
-        if m:
-            total_page += 1
+        # 分页设计:
+        page_object = Pagination(request, filter_ip_content=filter_ip_content, plus=10)
 
-        # 2. 生成页码
-        #   只显示前五页和后五页
-        if total_page < 11:
-            page_start = 1
-            page_end = total_page + 1
-        else:
-            if page < 6:
-                page_start = 1
-                page_end = 11
-            else:
-                if page + 5 > total_page:
-                    page_start = total_page - 9
-                    page_end = total_page + 1
-                else:
-                    page_start = page - 5
-                    page_end = page + 6
+        filter_ip_content = page_object.filter_ip_content
+        page_str = page_object.html()
+        total_page = page_object.total_page
+        page = page_object.page
 
-        page_str_list = []
+        # 传递数据字典
+        context = {'websites': filter_ip_content, 'filter_condition': filter_ip_condition,
+                   'type_choices': type_choices, 'page_str': page_str, 'total_page': total_page,
+                   'current_page': page}
 
-        # 首页
-        first = '<li><a href="?page=1"><span aria-hidden="true">首页</span></a></li>'
-        page_str_list.append(first)
-
-        # 上一页
-        if page == 1:
-            prev = '<li class="disabled"><a href="#"><span aria-hidden="true">&laquo;</span></a></li>'
-        else:
-            prev = '<li><a href="?page=%s"><span aria-hidden="true">&laquo;</span></a></li>' % (page - 1)
-        page_str_list.append(prev)
-
-        for i in range(page_start, page_end):
-            if i == page:
-                temp = '<li class="active"><a href="?page=%s">%s</a></li>' % (i, i)
-            else:
-                temp = '<li><a href="?page=%s">%s</a></li>' % (i, i)
-            page_str_list.append(temp)
-
-        # 下一页
-        if page == total_page:
-            after = '<li class="disabled"><a href="#"><span aria-hidden="true">&raquo;</span></a></li>'
-        else:
-            after = '<li><a href="?page=%s"><span aria-hidden="true">&raquo;</span></a></li>' % (page + 1)
-        page_str_list.append(after)
-
-        # 尾页
-        last = '<li><a href="?page=%s"><span aria-hidden="true">尾页</span></a></li>' % total_page
-        page_str_list.append(last)
-
-        """
-        <li><a href="?page=2#">2</a></li
-        <li><a href="?page=3">3</a></li>
-        <li><a href="?page=4">4</a></li>
-        <li><a href="?page=5">5</a></li>
-        """
-
-        # 跳转框
-        jump = """<li>
-                    <form method="get" style="float: left;margin-left: -1px">
-                        <input name="page"
-                               style="position: relative;float: left;display: inline-block;width: 100px;border-radius:0 "
-                               type="number" class="form-control" placeholder="页码">
-                        <button id="jump-button" class="btn btn-default" type="submit">跳转</button>
-                    </form>
-                </li>
-                """
-        page_str_list.append(jump)
-
-        page_str = mark_safe(''.join(page_str_list))
-
-        return render(request, 'fraud_ip_list.html',
-                      {'websites': filter_ip_content, 'filter_condition': filter_ip_condition,
-                       'type_choices': type_choices, 'page_str': page_str, 'total_page': total_page,
-                       'current_page': page})
+        return render(request, 'fraud_ip_list.html', context)
 
 
 def fraud_email_list(request):
